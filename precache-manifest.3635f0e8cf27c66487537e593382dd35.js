04a314c9a53013e62091c883b5408106539ab08e
Dima Doronin <doronindm@gmail.com>
2020-09-05 01:28:05 -0400

    Updates

---
+++ precache-manifest.3635f0e8cf27c66487537e593382dd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a95cacdcf962e0df00b536ac2fd9c0",
+    "url": "/map-console/index.html"
+  },
+  {
+    "revision": "50be423cec1792efc868",
+    "url": "/map-console/static/css/main.466245b7.chunk.css"
+  },
+  {
+    "revision": "634e1da2e9d41508d579",
+    "url": "/map-console/static/js/2.b57e7734.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/map-console/static/js/2.b57e7734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50be423cec1792efc868",
+    "url": "/map-console/static/js/main.493a61f4.chunk.js"
+  },
+  {
+    "revision": "7a03fe041364573071df",
+    "url": "/map-console/static/js/runtime-main.2a061612.js"
+  }
+]);
